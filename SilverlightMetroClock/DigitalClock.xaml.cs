92944dc1e6cdcccb99fe36ce83f5fd3deb40e153
Steven Hollidge <stevenhollidge@hotmail.com>
2012-06-22 20:50:43 +0100

    Initial commit

---
+++ SilverlightMetroClock/DigitalClock.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace SilverlightMetroClock
+{
+    public partial class DigitalClock
+    {
+
+        public DigitalClock()
+        {
+            InitializeComponent();
+
+            // updates every minute but perhaps not on the minute :)
+            DispatcherTimer timer = new DispatcherTimer {Interval = new TimeSpan(0, 0, 1, 0)};
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+            UpdateDisplay();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            UpdateDisplay();
+        }
+
+        void UpdateDisplay()
+        {
+            var now = DateTime.Now;
+
+            TimeTextBlock.Text = now.ToString("HH:mm");
+            DayTextBlock.Text = now.ToString("dddd");
+            DateTextBlock.Text = now.ToString("MMMM d");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Silverlight Metro Clock
+
+This is the source code that accompaniments the [blog post](http://stevenhollidge.blogspot.co.uk/2012/06/silverlight-metro-clock.html) by [Steven Hollidge](http://stevenhollidge.com).
+
+
+## About
+The aim is promote, share and cross pollinate ideas and approaches for data entry within Metro style apps.
+
+Source code:  [https://github.com/stevenh77/SilverlightMetroClock](https://github.com/stevenh77/SilverlightMetroClock)
+
+Live Demo:  [http://stevenhollidge.com/blog-source-code/silverlightmetroclock](http://stevenhollidge.com/blog-source-code/silverlightmetroclock)
